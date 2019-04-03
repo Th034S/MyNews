155cfce1bb995d781947a00d31643b482e2afebd
thomas <siadousthomas@gmail.com>
2019-04-03 19:36:34 +0200

    Add method of TopStoriesFragment, update topStories layout and create layout for an item, and create Adapter

---
+++ src/main/java/com/siadous/thomas/mynews/top_stories_list/ShowEmptyView.java
@@ -0,0 +1,7 @@
+package com.siadous.thomas.mynews.top_stories_list;
+
+public interface ShowEmptyView {
+    void showEmptyView();
+
+    void hideEmptyView();
+}
--- src/main/java/com/siadous/thomas/mynews/top_stories_list/TopStoriesFragment.java
+++ src/main/java/com/siadous/thomas/mynews/top_stories_list/TopStoriesFragment.java
@@ -1,16 +1,24 @@
 package com.siadous.thomas.mynews.top_stories_list;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.siadous.thomas.mynews.Adapters.TopStoriesAdapter;
 import com.siadous.thomas.mynews.Model.ResultList;
 import com.siadous.thomas.mynews.Model.Results;
 import com.siadous.thomas.mynews.Model.TopStories;
@@ -22,8 +30,26 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TopStoriesFragment extends Fragment implements TopStoriesContract.View  {
+public class TopStoriesFragment extends Fragment implements TopStoriesContract.View, ShowEmptyView  {
 
+    private static final String TAG = "TopStoriesFragment";
+    private TopStoriesPresenter topStoriesPresenter;
+    private RecyclerView rvTopStoriesList;
+    private List<TopStories> topStoriesList;
+    private TopStoriesAdapter topStoriesAdapter;
+    private ProgressBar pbLoading;
+    private TextView tvEmptyView;
+
+    public View result;
+
+    private int pageNo = 1;
+
+    //Constants for load more
+    private int previousTotal = 0;
+    private boolean loading = true;
+    private int visibleThreshold = 5;
+    int firstVisibleItem, visibleItemCount, totalItemCount;
+    private GridLayoutManager mLayoutManager;
 
     public static TopStoriesFragment newInstance() {
         return(new TopStoriesFragment());
@@ -34,6 +60,18 @@ public class TopStoriesFragment extends Fragment implements TopStoriesContract.V
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        result = inflater.inflate(R.layout.fragment_top_stories, container, false);
+
+        // Référencer les éléments graphique + créer une list de Movie
+        initUI();
+
+        setListeners();
+
+        // Initialiser le Presenter
+        topStoriesPresenter = new TopStoriesPresenter(this);
+        // Obtenir les données de la page 1
+        topStoriesPresenter.requestDataFromServer();
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_top_stories, container, false);
     }
@@ -41,41 +79,99 @@ public class TopStoriesFragment extends Fragment implements TopStoriesContract.V
 
     private void initUI() {
 
-        rvMovieList = findViewById(R.id.rv_movie_list);
+        rvTopStoriesList = result.findViewById(R.id.rv_top_stories_list);
 
-        moviesList = new ArrayList<>();
-        moviesAdapter = new MoviesAdapter(this, moviesList);
+        topStoriesList = new ArrayList<>();
+        topStoriesAdapter = new TopStoriesAdapter(this, topStoriesList);
 
         mLayoutManager = new GridLayoutManager(this, 2);
-        rvMovieList.setLayoutManager(mLayoutManager);
-        rvMovieList.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(this, 10), true));
-        rvMovieList.setItemAnimator(new DefaultItemAnimator());
-        rvMovieList.setAdapter(moviesAdapter);
+        rvTopStoriesList.setLayoutManager(mLayoutManager);
+        rvTopStoriesList.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(this, 10), true));
+        rvTopStoriesList.setItemAnimator(new DefaultItemAnimator());
+        rvTopStoriesList.setAdapter(topStoriesAdapter);
 
-        pbLoading = findViewById(R.id.pb_loading);
+        pbLoading = result.findViewById(R.id.pb_loading);
 
-        fabFilter = findViewById(R.id.fab_filter);
-
-        tvEmptyView = findViewById(R.id.tv_empty_view);
+        tvEmptyView = result.findViewById(R.id.tv_empty_view);
     }
+
+    /**
+     * This function will contain listeners for all views.
+     */
+    private void setListeners() {
+
+        rvTopStoriesList.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                visibleItemCount = rvTopStoriesList.getChildCount();
+                totalItemCount = mLayoutManager.getItemCount();
+                firstVisibleItem = mLayoutManager.findFirstVisibleItemPosition();
+
+                // Handling the infinite scroll
+                if (loading) {
+                    if (totalItemCount > previousTotal) {
+                        loading = false;
+                        previousTotal = totalItemCount;
+                    }
+                }
+                if (!loading && (totalItemCount - visibleItemCount)
+                        <= (firstVisibleItem + visibleThreshold)) {
+                    topStoriesPresenter.getMoreData(pageNo);
+                    loading = true;
+                }
+
+            }
+        });
+
+    }
+
 
     @Override
     public void showProgress() {
-
+        pbLoading.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void hideProgress() {
-
+        pbLoading.setVisibility(View.GONE);
     }
 
     @Override
-    public void setDataToRecyclerView(List<TopStories> topStoriesList) {
+    public void setDataToRecyclerView(List<TopStories> topStoriesArrayList) {
+        topStoriesList.addAll(topStoriesArrayList);
+        topStoriesAdapter.notifyDataSetChanged();
 
+        // This will help us to fetch data from next page no.
+        pageNo++;
     }
 
     @Override
     public void onResponseFailure(Throwable throwable) {
+        Log.e(TAG, throwable.getMessage());
+    }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        topStoriesPresenter.onDestroy();
+    }
+
+
+
+    @Override
+    public void showEmptyView() {
+
+        rvTopStoriesList.setVisibility(View.GONE);
+        tvEmptyView.setVisibility(View.VISIBLE);
+
+    }
+
+    @Override
+    public void hideEmptyView() {
+        rvTopStoriesList.setVisibility(View.VISIBLE);
+        tvEmptyView.setVisibility(View.GONE);
     }
 }
