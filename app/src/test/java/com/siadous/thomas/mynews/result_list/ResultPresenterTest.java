39a0b985c31cdcf440f3499b8bea78e8511bcf00
thomas <siadousthomas@gmail.com>
2020-01-04 18:14:47 +0100

    write unit test for ResultPresenter

--- app/src/test/java/com/siadous/thomas/mynews/result_list/ResultPresenterTest.java
+++ app/src/test/java/com/siadous/thomas/mynews/result_list/ResultPresenterTest.java
@@ -1,10 +1,16 @@
 package com.siadous.thomas.mynews.result_list;
 
+import com.siadous.thomas.mynews.model.ArticleSearch.Docs;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ResultPresenterTest {
 
@@ -22,7 +28,7 @@ public class ResultPresenterTest {
 
 
     @Test
-    public void getMoreData_callsModelGetResultList() {
+    public void getMoreDataWithoutDate_callsModelGetResultList() {
         int pageNb = 3;
         String keyword = "trump";
         String category = "arts, politics";
@@ -31,11 +37,100 @@ public class ResultPresenterTest {
     }
 
     @Test
-    public void requestDataFromServer_callsModelGetResultList() {
+    public void getMoreData_callsModelGetResultList() {
+        int pageNb = 3;
+        String keyword = "trump";
+        String category = "arts, politics";
+        int beginDate = 20190912;
+        int endDate = 20191009;
+        resultPresenter.getMoreData(pageNb, category, keyword, beginDate, endDate);
+        Mockito.verify(resultListModel).getResultList(resultPresenter, pageNb, category, keyword, beginDate, endDate);
+    }
+
+    @Test
+    public void requestDataFromServerWithoutDate_callsModelGetResultList() {
         String keyword = "trump";
         String category = "arts, politics";
         resultPresenter.requestDataFromServerWithoutDate(category, keyword);
         Mockito.verify(resultListModel).getResultListWithoutDate(resultPresenter, 1, category, keyword);
     }
+
+    @Test
+    public void requestDataFromServer_callsModelGetResultList() {
+        String keyword = "trump";
+        String category = "arts, politics";
+        int beginDate = 20190912;
+        int endDate = 20191009;
+        resultPresenter.requestDataFromServer(category, keyword, beginDate, endDate);
+        Mockito.verify(resultListModel).getResultList(resultPresenter, 1, category, keyword, beginDate, endDate);
+    }
+
+    @Test
+    public void getMoreDataWithoutDate_ReturnsSuccessWithResult() {
+        String keyword = "trump";
+        String category = "arts, politics";
+
+        Docs docs = new Docs();
+        docs.set_id("23");
+        docs.setSnippet("snippet");
+        docs.setPub_date("23-12-2019");
+        docs.setSource("bloomberg");
+
+        final List<Docs> docsList = new ArrayList<>();
+        docsList.add(docs);
+        docsList.add(docs);
+        docsList.add(docs);
+        docsList.add(docs);
+        docsList.add(docs);
+
+        Mockito.doAnswer(new Answer<ResultContract.Model.OnFinishedListener>() {
+            @Override
+            public ResultContract.Model.OnFinishedListener  answer(InvocationOnMock invocation) throws Throwable {
+                ResultContract.Model.OnFinishedListener callback = invocation.getArgument(0);
+                callback.onFinished(docsList);
+                return null;
+            }
+        }).when(resultListModel).getResultListWithoutDate(resultPresenter, 2, category, keyword);
+
+        resultPresenter.getMoreDataWithoutDate(2, category, keyword);
+
+        Mockito.verify(resultListView).setDataToRecyclerView(docsList);
+    }
+
+
+    @Test
+    public void getMoreData_ReturnsSuccessWithResult() {
+        String keyword = "trump";
+        String category = "arts, politics";
+        int beginDate = 20190912;
+        int endDate = 20191009;
+
+        Docs docs = new Docs();
+        docs.set_id("23");
+        docs.setSnippet("snippet");
+        docs.setPub_date("23-12-2019");
+        docs.setSource("bloomberg");
+
+        final List<Docs> docsList = new ArrayList<>();
+        docsList.add(docs);
+        docsList.add(docs);
+        docsList.add(docs);
+        docsList.add(docs);
+        docsList.add(docs);
+
+        Mockito.doAnswer(new Answer<ResultContract.Model.OnFinishedListener>() {
+            @Override
+            public ResultContract.Model.OnFinishedListener  answer(InvocationOnMock invocation) throws Throwable {
+                ResultContract.Model.OnFinishedListener callback = invocation.getArgument(0);
+                callback.onFinished(docsList);
+                return null;
+            }
+        }).when(resultListModel).getResultList(resultPresenter, 2, category, keyword, beginDate, endDate);
+
+        resultPresenter.getMoreData(2, category, keyword, beginDate, endDate);
+
+        Mockito.verify(resultListView).setDataToRecyclerView(docsList);
+    }
+
 
 }
